09d215591563fae03ec08aff7b5f5f068ad0119d
cosmin-marginean <cos@hey.com>
2023-07-05 13:12:38 +0100

    Cleanup

--- release.sh
+++ release.sh
@@ -5,16 +5,10 @@ set -e
 VERSION=`cat version.properties | grep "version" | awk -F' *= *' '{print $2}'`
 echo "Version is $VERSION"
 
-rm -rf docs/dokka
 export COVERALLS_REPO_TOKEN="${COVERALLS_REPO_TOKEN_SLINK}"
-./gradlew clean test dokkaHtml publish
+./gradlew publish
 
-git add --all
-git commit -am "Release $VERSION"
-git push
-git tag "${VERSION}"
+git tag "v${VERSION}" -m "Release v${VERSION}"
 git push --tags --force
-
-gh release create "${VERSION}" --verify-tag --title "Slink ${VERSION}" --notes "Version ${VERSION}"
 
 echo "Finished building version $VERSION"
